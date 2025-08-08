e135f8e1505a908071183391b42b8777e3efe940
AmirOmidid <omidi.amir@gmail.com>
2025-08-08 01:53:31 -0400

    new directories structure generated

--- tests/test_train.py
+++
@@ -1,7 +0,0 @@
-def test_training_script_runs():
-    """
-    This is a placeholder test to ensure pytest picks up train.py.
-    Integration tests for training would need mocked data, 
-    or short config to run quickly.
-    """
-    assert True
