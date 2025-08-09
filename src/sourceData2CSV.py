80efc1444a0c818f0af16b6959bf38428869fd68
AmirOmidid <omidi.amir@gmail.com>
2025-08-09 16:10:28 -0400

    Paths used in the sourceData2csv.py optimized

--- src/sourceData2CSV.py
+++ src/sourceData2CSV.py
@@ -1,13 +1,18 @@
 import pandas as pd
+import os
 
-en_path_train = "data/en-fr/opus.en-fr-train.en"
-fr_path_train = "data/en-fr/opus.en-fr-train.fr"
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+DATA_DIR = os.path.join(PROJECT_ROOT, "data")
 
-en_path_test = "data/en-fr/opus.en-fr-test.en"
-fr_path_test = "data/en-fr/opus.en-fr-test.fr"
 
-en_path_val = "data/en-fr/opus.en-fr-dev.en"
-fr_path_val = "data/en-fr/opus.en-fr-dev.fr"
+en_path_train = os.path.join(DATA_DIR,"en-fr","opus.en-fr-train.en")
+fr_path_train = os.path.join(DATA_DIR,"en-fr","opus.en-fr-train.fr")
+
+en_path_test = os.path.join(DATA_DIR,"en-fr","opus.en-fr-test.en")
+fr_path_test = os.path.join(DATA_DIR,"en-fr","opus.en-fr-test.fr")
+
+en_path_val = os.path.join(DATA_DIR,"en-fr","opus.en-fr-dev.en")
+fr_path_val = os.path.join(DATA_DIR,"en-fr","opus.en-fr-dev.fr")
 
 with open(en_path_train, encoding="utf-8") as f_en, open(fr_path_train, encoding="utf-8") as f_fr:
     en_lines_train = [line.strip() for line in f_en]
@@ -31,11 +36,11 @@ df_train = pd.DataFrame({'en': en_lines_train, 'fr': fr_lines_train})
 df_test = pd.DataFrame({'en': en_lines_test, 'fr': fr_lines_test})
 df_val = pd.DataFrame({'en': en_lines_val, 'fr': fr_lines_val})
 
-df_train.to_csv("data/opus100_en_fr_train.csv", index=False)
+df_train.to_csv(os.path.join(DATA_DIR,"opus100_en_fr_train.csv"), index=False)
 print("Saved to data/en-fr/opus100_en_fr_train.csv")
 
-df_test.to_csv("data/opus100_en_fr_test.csv", index=False)
+df_test.to_csv(os.path.join(DATA_DIR,"opus100_en_fr_test.csv"), index=False)
 print("Saved to data/en-fr/opus100_en_fr_test.csv")
 
-df_val.to_csv("data/opus100_en_fr_val.csv", index=False)
+df_val.to_csv(os.path.join(DATA_DIR,"opus100_en_fr_val.csv"), index=False)
 print("Saved to data/en-fr/opus100_en_fr_val.csv")
