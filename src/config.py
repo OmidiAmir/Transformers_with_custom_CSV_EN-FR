2a6e393089355e869591d5e9835195371452f427
AmirOmidid <omidi.amir@gmail.com>
2025-08-10 00:51:45 -0400

    configuration parametes updated!

--- src/config.py
+++ src/config.py
@@ -7,35 +7,29 @@ class Config:
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         
         self.PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-
-        self.DATA_DIR = os.path.join(self.PROJECT_ROOT, "data")
-        
+        self.DATA_DIR = os.path.join(self.PROJECT_ROOT, "data")        
         self.MODELS_DIR = os.path.join(self.PROJECT_ROOT, "models")
-
         self.TOK_DIR = os.path.join(self.PROJECT_ROOT, "tokenizerfiles")
+        self.checkpoint_dir = os.path.join(self.PROJECT_ROOT, 'checkpoints')
 
         # Dataset
         self.dataSetFileName = 'opus100_en_fr.csv'
         self.sourceLang = 'en'
         self.targetLang = 'fr'
-        self.trainPercentage = 0.9
 
         # Model settings
         self.d_model = 256
         self.num_heads = 4
         self.num_layers = 2
         self.d_ff = 512
-        self.dropout = 0.0
+        self.dropout = 0.1
         self.max_seq_length = 128
 
         # Training settings
         self.trainBatchSize = 32
         self.valBatchSize = 1
-        self.lr = 0.0007
-        self.num_epochs = 10
-
-        # Checkpoint path (optional now)
-        self.checkpoint_dir = os.path.join(self.PROJECT_ROOT, 'checkpoints')
+        self.lr = 0.0003
+        self.num_epochs = 3
 
         for p in [self.DATA_DIR, self.MODELS_DIR, self.TOK_DIR, self.checkpoint_dir]:
             os.makedirs(p, exist_ok=True)
